2d4f5a0b753d8d168bcf6abdac85c27584f8aaa4
lucasbatistao <lucassbatistao22@gmail.com>
2024-11-18 11:31:30 -0300

    RI-1
    Adicionados:
    
    - Realizado a criação do projeto base com as bibliotecas padrões.

--- windows/readit/ViewModels/ErroViewModel.cs
+++ windows/readit/ViewModels/ErroViewModel.cs
@@ -1,6 +1,37 @@
-﻿namespace readit.ViewModels
+﻿using Caliburn.Micro;
+
+namespace readit.ViewModels
 {
-    internal class ErroViewModel
+    public class ErroViewModel : Screen
     {
+        private string _imagem;
+
+        public string Imagem
+        {
+            get { return _imagem; }
+            set
+            {
+                _imagem = value;
+                NotifyOfPropertyChange(() => Imagem);
+            }
+        }
+
+        private string _mensagem;
+
+        public string Mensagem
+        {
+            get { return _mensagem; }
+            set
+            {
+                _mensagem = value;
+                NotifyOfPropertyChange(() => Mensagem);
+            }
+        }
+
+        public ErroViewModel(string imagem, string mensagem)
+        {
+            Imagem = imagem;
+            Mensagem = mensagem;
+        }
     }
 }
--- windows/readit/Views/ShellView.xaml.cs
+++ windows/readit/Views/ShellView.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
+using System.Windows;
 
 namespace readit.Views
 {
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ShellView : Window
+    public partial class ShellView : MetroWindow
     {
         public ShellView()
         {
